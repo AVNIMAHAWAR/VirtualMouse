9bbd1c9bc814e91a3868a7f7ea8d8969bdc8c872
AVNIMAHAWAR <avnimahawar2004@gmail.com>
2025-08-04 15:40:37 +0530

    Giving Camera Access

---
+++ VirtualMouse/main.py
@@ -0,0 +1,2 @@
+import cv2                                   #(1)video capturing
+cap=cv2.VideoCapture(0)                      #(2)
--- facedetect/VirtualMouse.py
+++
@@ -1,5 +0,0 @@
-import cv2                                   #(1)video capturing
-cap=cv2.VideoCapture(0)                      #(2)
-while True:                                  #(3)
-    _, frame = cap.read()                    #(4)
-    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #(5) rgb or greyscale conversion we can do, convertColor
