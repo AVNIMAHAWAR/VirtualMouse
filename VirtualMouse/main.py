7b8a0e9ffd23929fac5bd26c31b8643b4ea54d86
AVNIMAHAWAR <avnimahawar2004@gmail.com>
2025-08-05 13:36:39 +0530

    Reading image

--- VirtualMouse/main.py
+++ VirtualMouse/main.py
@@ -1,2 +1,7 @@
 import cv2                                   #(1)video capturing
 cap=cv2.VideoCapture(0)                      #(2)
+while True:                                  #(3)
+    _, frame = cap.read()                    #(4)
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #(5) rgb or greyscale conversion we can do, convertColor
+    cv2.imshow('Virtual Mouse',frame)                         #(6)to show the img
+    cv2.waitKey(1)                                            #(7)
